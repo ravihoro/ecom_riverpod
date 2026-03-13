29d35412f12a43fe3d7d60aa2c7c37e30920861e
ravihoro <ravijohnhoro5@gmail.com>
2026-03-13 22:22:49 +0530

    Added project

---
+++ lib/core/error/exceptions.dart
@@ -0,0 +1,17 @@
+sealed class AppException implements Exception {
+  final String message;
+
+  const AppException(this.message);
+}
+
+class ServerException extends AppException {
+  const ServerException(super.message);
+}
+
+class NetworkException extends AppException {
+  const NetworkException(super.message);
+}
+
+class AuthException extends AppException {
+  const AuthException(super.message);
+}
---
+++ lib/core/error/failure.dart
@@ -0,0 +1,17 @@
+sealed class Failure {
+  final String message;
+
+  const Failure(this.message);
+}
+
+class ServerFailure extends Failure {
+  const ServerFailure(super.message);
+}
+
+class NetworkFailure extends Failure {
+  const NetworkFailure(super.message);
+}
+
+class AuthFailure extends Failure {
+  const AuthFailure(super.message);
+}
