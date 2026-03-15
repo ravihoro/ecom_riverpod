f4af00bff5ea7ceb014c72077bd64c51c678af72
ravihoro <ravijohnhoro5@gmail.com>
2026-03-15 22:04:19 +0530

    Fix: Updated router to take in ref

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,57 +1,67 @@
 import 'package:ecom_riverpod/core/navigation/app_navigation.dart';
+import 'package:ecom_riverpod/features/auth/presentation/page/sign_in_page.dart';
 import 'package:ecom_riverpod/features/home/presentation/page/home_page.dart';
 import 'package:ecom_riverpod/core/router/route_names.dart';
 import 'package:ecom_riverpod/core/router/route_paths.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
-final GoRouter appRouter = GoRouter(
-  initialLocation: RoutePaths.home,
-  routes: [
-    StatefulShellRoute.indexedStack(
-      builder: (_, state, navigationShell) {
-        return AppNavigation(navigationShell: navigationShell);
-      },
-      branches: [
-        StatefulShellBranch(
-          routes: [
-            GoRoute(
-              path: RoutePaths.home,
-              name: RouteNames.home,
-              builder: (_, _) => HomePage(),
-            ),
-          ],
-        ),
+GoRouter appRouter(Ref ref) {
+  return GoRouter(
+    initialLocation: RoutePaths.home,
+    redirect: (context, state) {},
+    routes: [
+      GoRoute(
+        path: RoutePaths.login,
+        name: RouteNames.login,
+        builder: (_, _) => SignInPage(),
+      ),
+      StatefulShellRoute.indexedStack(
+        builder: (_, state, navigationShell) {
+          return AppNavigation(navigationShell: navigationShell);
+        },
+        branches: [
+          StatefulShellBranch(
+            routes: [
+              GoRoute(
+                path: RoutePaths.home,
+                name: RouteNames.home,
+                builder: (_, _) => HomePage(),
+              ),
+            ],
+          ),
 
-        StatefulShellBranch(
-          routes: [
-            GoRoute(
-              path: RoutePaths.category,
-              name: RouteNames.category,
-              builder: (_, _) => HomePage(),
-            ),
-          ],
-        ),
+          StatefulShellBranch(
+            routes: [
+              GoRoute(
+                path: RoutePaths.category,
+                name: RouteNames.category,
+                builder: (_, _) => HomePage(),
+              ),
+            ],
+          ),
 
-        StatefulShellBranch(
-          routes: [
-            GoRoute(
-              path: RoutePaths.cart,
-              name: RouteNames.cart,
-              builder: (_, _) => HomePage(),
-            ),
-          ],
-        ),
+          StatefulShellBranch(
+            routes: [
+              GoRoute(
+                path: RoutePaths.cart,
+                name: RouteNames.cart,
+                builder: (_, _) => HomePage(),
+              ),
+            ],
+          ),
 
-        StatefulShellBranch(
-          routes: [
-            GoRoute(
-              path: RoutePaths.profile,
-              name: RouteNames.profile,
-              builder: (_, _) => HomePage(),
-            ),
-          ],
-        ),
-      ],
-    ),
-  ],
-);
+          StatefulShellBranch(
+            routes: [
+              GoRoute(
+                path: RoutePaths.profile,
+                name: RouteNames.profile,
+                builder: (_, _) => SignInPage(),
+              ),
+            ],
+          ),
+        ],
+      ),
+    ],
+  );
+}
--- lib/core/router/router_provider.dart
+++ lib/core/router/router_provider.dart
@@ -3,5 +3,5 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
 final routerProvider = Provider<GoRouter>((ref) {
-  return appRouter;
+  return appRouter(ref);
 });
