e99261a00ebd8eea97890140c135024a26d75cf1
ravihoro <ravijohnhoro5@gmail.com>
2026-03-15 18:22:44 +0530

    Feat: Added selected index indicator for bottom nav

--- lib/core/navigation/app_navigation.dart
+++ lib/core/navigation/app_navigation.dart
@@ -18,47 +18,77 @@ class AppNavigation extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: navigationShell,
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: navigationShell.currentIndex,
-        type: BottomNavigationBarType.fixed,
-        selectedItemColor: AppColors.primary,
-        unselectedItemColor: Colors.grey,
-        onTap: _onTap,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(
-              navigationShell.currentIndex == 0
-                  ? Icons.home
-                  : Icons.home_outlined,
-            ),
-            label: 'Home',
+      bottomNavigationBar: Stack(
+        children: [
+          BottomNavigationBar(
+            backgroundColor: Colors.white,
+            currentIndex: navigationShell.currentIndex,
+            type: BottomNavigationBarType.fixed,
+            selectedItemColor: AppColors.primary,
+
+            unselectedItemColor: Colors.grey,
+            onTap: _onTap,
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(
+                  navigationShell.currentIndex == 0
+                      ? Icons.home
+                      : Icons.home_outlined,
+                ),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  navigationShell.currentIndex == 1
+                      ? Icons.grid_view
+                      : Icons.grid_view_outlined,
+                ),
+                label: 'Category',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  navigationShell.currentIndex == 2
+                      ? Icons.shopping_cart
+                      : Icons.shopping_cart_outlined,
+                ),
+                label: 'Cart',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  navigationShell.currentIndex == 3
+                      ? Icons.person
+                      : Icons.person_outlined,
+                ),
+                label: 'Profile',
+              ),
+            ],
           ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              navigationShell.currentIndex == 1
-                  ? Icons.grid_view
-                  : Icons.grid_view_outlined,
-            ),
-            label: 'Category',
+          Container(
+            width: double.infinity,
+            height: 2,
+            color: AppColors.primary.shade50,
           ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              navigationShell.currentIndex == 2
-                  ? Icons.shopping_cart
-                  : Icons.shopping_cart_outlined,
-            ),
-            label: 'Cart',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              navigationShell.currentIndex == 3
-                  ? Icons.person
-                  : Icons.person_outlined,
-            ),
-            label: 'Profile',
-          ),
+          AnimatedSelectedIndexIndicator(index: navigationShell.currentIndex),
         ],
       ),
+    );
+  }
+}
+
+class AnimatedSelectedIndexIndicator extends StatelessWidget {
+  final int index;
+
+  const AnimatedSelectedIndexIndicator({super.key, required this.index});
+
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.sizeOf(context).width / 4;
+
+    return AnimatedPositioned(
+      duration: const Duration(milliseconds: 250),
+      curve: Curves.easeOut,
+      left: width * index,
+      child: Container(width: width, height: 2, color: AppColors.primary),
     );
   }
 }
