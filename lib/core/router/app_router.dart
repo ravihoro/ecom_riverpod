c1f1f2b3b197de3db7f9a955e75b6f955e701015
ravihoro <ravijohnhoro5@gmail.com>
2026-03-15 22:51:17 +0530

    Fix: Removed ref

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -6,62 +6,60 @@ import 'package:ecom_riverpod/core/router/route_paths.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
-GoRouter appRouter(Ref ref) {
-  return GoRouter(
-    initialLocation: RoutePaths.home,
-    redirect: (context, state) {},
-    routes: [
-      GoRoute(
-        path: RoutePaths.login,
-        name: RouteNames.login,
-        builder: (_, _) => SignInPage(),
-      ),
-      StatefulShellRoute.indexedStack(
-        builder: (_, state, navigationShell) {
-          return AppNavigation(navigationShell: navigationShell);
-        },
-        branches: [
-          StatefulShellBranch(
-            routes: [
-              GoRoute(
-                path: RoutePaths.home,
-                name: RouteNames.home,
-                builder: (_, _) => HomePage(),
-              ),
-            ],
-          ),
+final appRouter = GoRouter(
+  initialLocation: RoutePaths.home,
+  redirect: (context, state) {},
+  routes: [
+    GoRoute(
+      path: RoutePaths.login,
+      name: RouteNames.login,
+      builder: (_, _) => SignInPage(),
+    ),
+    StatefulShellRoute.indexedStack(
+      builder: (_, state, navigationShell) {
+        return AppNavigation(navigationShell: navigationShell);
+      },
+      branches: [
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: RoutePaths.home,
+              name: RouteNames.home,
+              builder: (_, _) => HomePage(),
+            ),
+          ],
+        ),
 
-          StatefulShellBranch(
-            routes: [
-              GoRoute(
-                path: RoutePaths.category,
-                name: RouteNames.category,
-                builder: (_, _) => HomePage(),
-              ),
-            ],
-          ),
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: RoutePaths.category,
+              name: RouteNames.category,
+              builder: (_, _) => HomePage(),
+            ),
+          ],
+        ),
 
-          StatefulShellBranch(
-            routes: [
-              GoRoute(
-                path: RoutePaths.cart,
-                name: RouteNames.cart,
-                builder: (_, _) => HomePage(),
-              ),
-            ],
-          ),
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: RoutePaths.cart,
+              name: RouteNames.cart,
+              builder: (_, _) => HomePage(),
+            ),
+          ],
+        ),
 
-          StatefulShellBranch(
-            routes: [
-              GoRoute(
-                path: RoutePaths.profile,
-                name: RouteNames.profile,
-                builder: (_, _) => SignInPage(),
-              ),
-            ],
-          ),
-        ],
-      ),
-    ],
-  );
-}
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: RoutePaths.profile,
+              name: RouteNames.profile,
+              builder: (_, _) => SignInPage(),
+            ),
+          ],
+        ),
+      ],
+    ),
+  ],
+);
