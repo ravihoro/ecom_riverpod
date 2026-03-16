7b620d49856c15c653254de36a61365fd462872d
ravihoro <ravijohnhoro5@gmail.com>
2026-03-16 10:54:52 +0530

    Fix: Using abstraction

--- lib/features/auth/data/repositories/auth_repository_impl.dart
+++ lib/features/auth/data/repositories/auth_repository_impl.dart
@@ -3,13 +3,12 @@ import 'package:ecom_riverpod/core/error/exceptions.dart';
 import 'package:ecom_riverpod/core/error/failure.dart';
 import 'package:ecom_riverpod/core/storage/token_storage.dart';
 import 'package:ecom_riverpod/features/auth/data/datasources/auth_data_source.dart';
-import 'package:ecom_riverpod/features/auth/data/datasources/auth_remote_data_source.dart';
 import 'package:ecom_riverpod/features/auth/data/models/auth_response_model.dart';
 import 'package:ecom_riverpod/features/auth/domain/entities/auth_session.dart';
 import 'package:ecom_riverpod/features/auth/domain/repositories/auth_repository.dart';
 
 class AuthRepositoryImpl extends AuthRepository {
-  final AuthRemoteDataSource remoteDataSource;
+  final AuthDataSource remoteDataSource;
   final TokenStorage tokenStorage;
 
   AuthRepositoryImpl(this.remoteDataSource, this.tokenStorage);
--- lib/features/auth/providers/auth_providers.dart
+++ lib/features/auth/providers/auth_providers.dart
@@ -1,11 +1,12 @@
 import 'package:ecom_riverpod/core/network/dio_provider.dart';
 import 'package:ecom_riverpod/core/storage/token_storage_provider.dart';
+import 'package:ecom_riverpod/features/auth/data/datasources/auth_data_source.dart';
 import 'package:ecom_riverpod/features/auth/data/datasources/auth_remote_data_source.dart';
 import 'package:ecom_riverpod/features/auth/data/repositories/auth_repository_impl.dart';
 import 'package:ecom_riverpod/features/auth/domain/repositories/auth_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final authRemoteDataSourceProvider = Provider<AuthRemoteDataSource>((ref) {
+final authRemoteDataSourceProvider = Provider<AuthDataSource>((ref) {
   final dio = ref.read(dioProvider);
 
   return AuthRemoteDataSource(dio);
