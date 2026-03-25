c04ec0e830ab0b60fac82d96f5908ba2e5a68d93
ravihoro <ravijohnhoro5@gmail.com>
2026-03-25 22:59:20 +0530

    Fix: Added new size

--- lib/core/design_system/app_sizes.dart
+++ lib/core/design_system/app_sizes.dart
@@ -1,6 +1,7 @@
 abstract class AppSizes {
   static const xs = 4.0;
   static const sm = 8.0;
+  static const msm = 12.0;
   static const md = 16.0;
   static const lg = 24.0;
   static const xl = 32.0;
