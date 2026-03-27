4e081764b6ff4882e7e22416ee0c12815dec610a
ravihoro <ravijohnhoro5@gmail.com>
2026-03-27 12:05:03 +0530

    Feat: Added interceptor for header

--- lib/core/network/dio_provider.dart
+++ lib/core/network/dio_provider.dart
@@ -1,13 +1,33 @@
 import 'package:dio/dio.dart';
 import 'package:ecom_riverpod/core/constants/api_endpoints.dart';
+import 'package:ecom_riverpod/features/auth/providers/auth_providers.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final dioProvider = Provider<Dio>((ref) {
-  return Dio(
+  final dio = Dio(
     BaseOptions(
       baseUrl: ApiEndpoints.baseUrl,
       connectTimeout: const Duration(seconds: 10),
       receiveTimeout: const Duration(seconds: 10),
     ),
   );
+
+  final localDataSource = ref.read(authLocalDataSourceProvider);
+
+  dio.interceptors.add(
+    InterceptorsWrapper(
+      onRequest: (options, handler) async {
+        final token = await localDataSource.getAccessToken();
+
+        if (token != null) {
+          print('token: $token');
+          options.headers['Authorization'] = 'Bearer $token';
+        }
+
+        handler.next(options);
+      },
+    ),
+  );
+
+  return dio;
 });
