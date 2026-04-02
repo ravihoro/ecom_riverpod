24a72ecd87bc672cec15c295d555d4753e1c765a
ravihoro <ravijohnhoro5@gmail.com>
2026-04-02 10:45:15 +0530

    Fix: Added container height

--- lib/features/home/presentation/components/banner_carousel.dart
+++ lib/features/home/presentation/components/banner_carousel.dart
@@ -21,27 +21,30 @@ class BannerCarousel extends StatelessWidget {
           height: 150,
           child: SingleChildScrollView(
             scrollDirection: Axis.horizontal,
-            child: Row(
-              children: images.map((e) {
-                int index = images.indexOf(e);
+            child: IntrinsicHeight(
+              child: Row(
+                children: images.map((e) {
+                  int index = images.indexOf(e);
 
-                final isFirst = index == 0;
-                final isLast = index == 2;
-                return Container(
-                  clipBehavior: Clip.antiAlias,
-                  decoration: BoxDecoration(borderRadius: AppRadius.sm),
-                  margin: isFirst
-                      ? EdgeInsets.only(left: padding)
-                      : isLast
-                      ? EdgeInsets.only(right: padding)
-                      : EdgeInsets.symmetric(horizontal: padding),
-                  child: CachedNetworkImage(
-                    fit: BoxFit.cover,
-                    imageUrl: e,
-                    width: width,
-                  ),
-                );
-              }).toList(),
+                  final isFirst = index == 0;
+                  final isLast = index == 2;
+                  return Container(
+                    height: 150,
+                    clipBehavior: Clip.antiAlias,
+                    decoration: BoxDecoration(borderRadius: AppRadius.sm),
+                    margin: isFirst
+                        ? EdgeInsets.only(left: padding)
+                        : isLast
+                        ? EdgeInsets.only(right: padding)
+                        : EdgeInsets.symmetric(horizontal: padding),
+                    child: CachedNetworkImage(
+                      fit: BoxFit.cover,
+                      imageUrl: e,
+                      width: width,
+                    ),
+                  );
+                }).toList(),
+              ),
             ),
           ),
         ),
